6dac39c384f373733a5bf2978bf5eba7cc41a4be
Jérôme Gorin <gorinje@gmail.com>
2026-09-01 16:20:37 +0200

    Remove dyn_call

--- dmx_avi.c
+++ dmx_avi.c
@@ -737,7 +737,7 @@ GF_FilterRegister AVIDmxRegister = {
 
 #endif // GPAC_DISABLE_AVILIB
 
-const GF_FilterRegister * EMSCRIPTEN_KEEPALIVE dynCall_avidmx_register(GF_FilterSession *session)
+const GF_FilterRegister * EMSCRIPTEN_KEEPALIVE avidmx_register(GF_FilterSession *session)
 {
 #ifndef GPAC_DISABLE_AVILIB
 	return &AVIDmxRegister;
@@ -748,5 +748,5 @@ const GF_FilterRegister * EMSCRIPTEN_KEEPALIVE dynCall_avidmx_register(GF_Filter
 #include "filter_register.h"
 __attribute__((constructor))
 void register_avidmx(void) {
-    gf_filter_auto_register("avidmx", dynCall_avidmx_register);
+    gf_filter_auto_register("avidmx", avidmx_register);
 }
